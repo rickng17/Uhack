bcf00d987d080631384cc6338f97552f0e0488ea
Maverick <maverick.mescolare@gmail.com>
2018-06-30 01:41:12 +0800

    add view edit account

--- app/src/main/java/com/example/maverick/uhack/LoginActivity.java
+++ app/src/main/java/com/example/maverick/uhack/LoginActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button login;
     private TextView signup;
 
     @Override
@@ -16,11 +18,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         signup = findViewById(R.id.link_signup);
+        login = findViewById(R.id.btn_login);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getBaseContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AccountActivity.class);
                 startActivity(intent);
             }
         });
